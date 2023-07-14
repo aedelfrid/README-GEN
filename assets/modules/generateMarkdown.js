6dfcd5f945c10c0cea20bf204f281c794563d8e2
aedelfrid <almck51@gmail.com>
2023-07-13 23:06:05 -0300

    added markdown generation algorithm

--- assets/modules/generateMarkdown.js
+++ assets/modules/generateMarkdown.js
@@ -1,6 +1,6 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {`[![MIT License][license-shield]][license-url]`}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,10 +12,98 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `
-  # ${data.title}
+  const { 
+    sectionsIncl, title, description, installation, usage, contributing,
+    tests
+  } = data
 
+  
+/////////////////////////////////////////
+  const intro = `
+# ${title}
+  
+  ## Description
+    
+    ${description}
+    
+  ## Table of Contents
+
+    [${sectionsIncl}](#${sectionsIncl})
   `;
-}
+
+//////////////////////////////////////////
+  const install = `
+        
+  ## Installation
+        
+    ${installation}
+  `
+////////////////////////////////////////////
+  const usageIns = `
+        
+  ## Usage
+        
+    ${usage}
+  `
+/////////////////////////////////////////////
+  const questionsContact = `
+        
+  ## Questions
+        
+  Please direct questions to the following;
+    ${github} ${githubLink}
+      
+    ${email}
+  `
+///////////////////////////////////////////////
+  const contributeIns = `
+        
+  ## Authors
+        
+  Contributions are what make the open source community such an amazing place to learn,<br>
+  inspire, and create. Any contributions you make are greatly appreciated.<br>
+
+  If you have a suggestion that would make this better, please fork the repo and create a <br>
+  pull request. You can also simply open an issue with the tag "enhancement". <br>
+  Don't forget to give the project a star! Thanks again!<br>
+     
+  ${contributing}
+  `
+
+//////////////////////////////////////////////////
+
+const testsIns = `
+    
+  ## Testing
+    
+  ${tests}
+`
+  
+// I know the above section is terrible and nigh unreadable but
+// this is the only way it works the way it want it to.
+
+  const formattedMD = intro
+
+  if (installation) {
+     formattedMD + install
+  };
+
+  if (usage) {
+    formattedMD + usageIns
+  };
+
+  formattedMD + questionsContact;
+
+  if (contributing) {
+    formattedMD + contributeIns
+  };
+
+  if (test) {
+    formattedMD + testsIns
+  };
+
+  return formattedMD
+
+};
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer')
 const fs = require('fs')
+const generateMarkdown = require('./assets/modules/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -12,9 +13,9 @@ const questions = [
     {   
         when: (answers) => answers.start == true,
         type: 'checkbox',
-        name: 'sections to include',
+        name: 'sectionsIncl',
         message: 'What sections would you you like to include? (Note: The description and License are included by default)',
-        choices: ['Installation','Badges', 'Usage','Support','Roadmap', 'Contributing','Authors']
+        choices: ['Installation','Usage','Contributing','Tests']
     },
     {
         when: (answers) => answers.start == true,
@@ -27,31 +28,103 @@ const questions = [
         type: 'input',
         name: 'description',
         message: 'What does your project do?'
-    }
+    },
+    {
+        when: (answers) => {
+            return answers.start == true && 
+                answers.sectionsIncl.includes('Installation')
+        },
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project?'
+    },
+    {
+        when: (answers) => {
+            return answers.start == true && 
+                answers.sectionsIncl.includes('Usage')
+        },
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?'
+    },
+    {
+        when: (answers) => {
+            return answers.start == true
+        },
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        when: (answers) => {
+            return answers.start == true
+        },
+        type: 'input',
+        name: 'githubLink',
+        message: 'Please provide a link to your GitHub Profile'
+    },
+    {
+        when: (answers) => {
+            return answers.start == true
+        },
+        type: 'input',
+        name: 'email',
+        message: 'What is your Email?'
+    },
+    {
+        when: (answers) => {
+            return answers.start == true && 
+                answers.sectionsIncl.includes('Contributing')
+        },
+        type: 'input',
+        name: 'contributing',
+        message: 'What is the best way to contribute to your project?'
+    },
+    {
+        when: (answers) => {
+            return answers.start == true && 
+                answers.sectionsIncl.includes('Tests')
+        },
+        type: 'input',
+        name: 'tests',
+        message: 'What is the best way to test your project?'
+    },
+    {
+        when: (answers) => {
+            return answers.start == true
+        },
+        type: 'checkbox',
+        name: 'licenseSelect',
+        message: 'Which license would you like to use?',
+        choices: ['MIT','Mozilla Public License 2.0','GNU AGPLv3','The Unlicense']
+    },
 ];
 
 // inquirer function
 
 const askQuestion = async () => {
-    inquirer.prompt(questions).then((answers) => {
+    inquirer.prompt(questions)
+        .then((answers) => {
             console.log(answers)
-            return answers
+            const md = generateMarkdown(answers)
+            
+            writeToFile(answers.title, md)
         })
-};
 
-const sectionsHandler = async (data) => {
-    if (await data._canBegin == true) {
-        data.sectionsIncl = askQuestion(questions[1]);
-    }
 };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    fs.appendFile(`./generated-assets/${fileName}.md`, data, (err) => {
+        if (err) throw err
+        else console.log('Saved');
+    });
 };
 
 // TODO: Create a function to initialize app
-(async function init() {
-    await askQuestion();
-})();
+function init() {
+    askQuestion();
+};
 // Function call to initialize app
+
+init();
